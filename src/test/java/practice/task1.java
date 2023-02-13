11e32d9e7b0e4160a8c1d30d3c8abbbf810cd523
prasanna-messi <np373111@gmail.com>
2023-02-13 12:23:03 +0530

    First time push

---
+++ src/test/java/practice/task1.java
@@ -0,0 +1,18 @@
+package practice;
+
+public class task1 {
+
+	private void module1() {
+		System.out.println("1");
+
+	}
+	private void module2() {
+	
+		System.out.println("2");
+	}
+	private void module3() {
+		System.out.println("3");
+	}
+	
+
+}
